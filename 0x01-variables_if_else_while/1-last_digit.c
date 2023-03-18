57482f4b6b3ee48841f8faf17fce24a42396b00f
Asmaa Ghalib <asmaeghalib@gmail.com>
2023-03-17 19:20:18 -0700

    cwr

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,12 +3,6 @@
 /* more headers goes there */
 #include<stdio.h>
 /* betty style doc for function main goes there */
-/**
- * main - main function
- *
- * Return: always 0
- */
-
 int main(void)
 {
 	int n;
@@ -16,12 +10,5 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive", n);
-	else if (n == 0)
-		printf("%d is zero", n);
-	else
-		printf("%d is negative", n);
-	print("\n");
 	return (0);
 }
