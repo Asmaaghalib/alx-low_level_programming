e79a860f702936dc40ba30e1b75377cb5c55a9e1
Asmaa Ghalib <asmaeghalib@gmail.com>
2023-03-19 11:08:09 -0700

    taask2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Return: always 0
+ * Return: always 0 
  */
 
 int main(void)
@@ -12,6 +12,6 @@ int main(void)
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
