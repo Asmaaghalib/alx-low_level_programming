a30bd03c855ec3224f4c1db210b93f108b11f83c
Asmaa Ghalib <asmaeghalib@gmail.com>
2023-03-22 17:39:28 -0700

    pdd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - to print the phrase wanted
+ * Return: 0 ode runs with success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
